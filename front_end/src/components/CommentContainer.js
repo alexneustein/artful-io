5fcc8471679b5b134cda733d0632ed5423ba4691
alexneustein <alex.neustein@gmail.com>
2018-09-11 16:51:56 -0400

    Added image detail functionality

---
+++ front_end/src/components/CommentContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Container, Header, Comment } from 'semantic-ui-react'
+import PostComment from './PostComment'
+
+
+
+class CommentContainer extends Component {
+
+  render() {
+    return (
+      <Container >
+        <Header as='h3'> Post Comments:</Header>
+        <Comment.Group size='large'>
+          { this.props.commentArray.map(comment => {
+            return <PostComment comment={comment} />
+          }) }
+        </Comment.Group>
+      </Container>
+    );
+  }
+
+}
+
+
+export default CommentContainer;
+
+CommentContainer.defaultProps = {
+  commentArray: ['There are no comments.']
+};
--- front_end/src/components/GalleryPost.js
+++ front_end/src/components/GalleryPost.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import { Container, Image, Header, Comment } from 'semantic-ui-react'
-import PostComment from './PostComment'
+import { Container, Image, Header, Comment, Card, Icon } from 'semantic-ui-react'
+import CommentContainer from './CommentContainer'
 
 class GalleryPost extends Component {
+  state = {
+    imageobj: {}
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/images/3')
+    .then(res => res.json())
+    .then(this.initialState)
+  }
+
+  initialState = (resData) => {
+    this.setState({
+      imageobj: resData,
+    })
+  }
+
+  dateFormatter = (datestring) => {
+    const dateMonths = ["","January","February","March","April","May","June","July","August","September","November","December"]
+    if (!!datestring) {
+      let dateArray = datestring.split('-');
+      const thestring = dateMonths[parseInt(dateArray[1])] + ", " + dateArray[0]
+      return thestring
+    }
+  }
 
   render() {
     return (
       <Container>
-        <Header as='h2'> Post Title </Header>
-        <Image src='https://picsum.photos/200/300/?random' size='massive' rounded />
-        <Container >
-          <Header as='h3'> Post Comments:</Header>
-          <Comment.Group size='large'>
-            <PostComment />
-            <PostComment />
-          </Comment.Group>
-        </Container>
+        <Header as='h2'>Image Detail</Header>
+        <Image src={this.state.imageobj.url} size='massive' rounded />
+        <Header as='h3'>Title: {this.state.imageobj.title}</Header>
+          <Card.Content extra>
+            <a>
+              <Icon name='like' />
+              {this.state.imageobj.likes} Likes
+            </a>
+          </Card.Content>
+        <p><b>Date:</b> {this.dateFormatter(this.state.imageobj.image_date)}</p>
+        <p><b>Details:</b> {this.state.imageobj.details}</p>
+        <CommentContainer commentArray={this.state.imageobj.comments} />
       </Container>
     );
   }
