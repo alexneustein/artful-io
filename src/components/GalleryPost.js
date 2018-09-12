b6e986e9b6efd1cb0d3a0a91defa5b5401990c87
alexneustein <alex.neustein@gmail.com>
2018-09-12 09:21:30 -0400

    fsdf

--- src/components/GalleryPost.js
+++ src/components/GalleryPost.js
@@ -12,7 +12,7 @@ class GalleryPost extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/images/3')
+    fetch('http://localhost:3001/images/3948')
     .then(res => res.json())
     .then(this.initialState)
   }
